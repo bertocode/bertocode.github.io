cf53e76d687f0edb053def736e684f061064b84f
Berto <bertocode@gmail.com>
2022-06-27 20:52:36 +0200

    Updates

--- index.js
+++ index.js
@@ -3,10 +3,37 @@
 /** @type ElmPagesInit */
 export default {
   load: async function (elmLoaded) {
+    applyDarkModeClass();
     const app = await elmLoaded;
-    console.log("App loaded", app);
+    app.ports.toggleDarkMode.subscribe(toggleDarkMode);
   },
   flags: function () {
     return "You can decode this in Shared.elm using Json.Decode.string!";
   },
 };
+
+function isDarkMode() {
+  return (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  );
+}
+
+function toggleDarkMode() {
+  if (isDarkMode()) {
+    localStorage.setItem("theme", "light");
+  } else {
+    localStorage.setItem("theme", "dark");
+  }
+  applyDarkModeClass();
+}
+function applyDarkModeClass() {
+  if (isDarkMode()) {
+    document.documentElement.classList.add("dark");
+    document.documentElement.classList.remove("light");
+  } else {
+    document.documentElement.classList.add("light");
+    document.documentElement.classList.remove("dark");
+  }
+}
